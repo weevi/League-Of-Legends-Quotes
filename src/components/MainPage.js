005770bfad5016104bf18464bcbb6c3ab5cadacf
weevi <paskeviciute.rima@gmail.com>
2019-07-16 12:33:58 +0000

    add styles

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,35 +1,34 @@
 import React from 'react'
+import ChampionsData from '../data/champions.json'
+import MainPageStyle from '../MainPageStyle.css'
+import "typeface-unica-one";
 
 class MainPage extends React.Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      character: {}
-    };
-  }
-
-  componentDidMount() {
-    fetch('http://ddragon.leagueoflegends.com/cdn/6.24.1/data/en_US/champion.json')
-      .then(results => {
-        return results.json();
-      })
-      .then(data => {
-        this.setState({
-        character: data
-        });
-      })
-  }
 render(){
-  return(
-    <div className="container2">
-      <div className="container1">
-        {this.state.character.type}
-      </div>
-    </div>
-  )
-}
+const keys = Object.keys(ChampionsData)
+const randomKey = Math.floor(Math.random() * keys.length)
+const champObject = ChampionsData[randomKey]
+const obj = Object.entries(champObject).map(([key, value]) => ({key, value}));
+const champData = [];
+Object.keys(champObject).forEach(key => champData.push({name: key, value: champObject[key]}))
 
+  return(
+<div>
+  <div className="wrap">
+  <h1>What is this champions name?</h1>
+   <div>
+        <img alt = "champion"src={champData[6].value}/>
+  </div>
+  <form>
+  <label>
+    <input type="text" name="name" placeholder="ENTER HERE"/>
+  </label>
+</form>
+</div>
+</div>
+  )
+ }
 }
 
 export default MainPage
